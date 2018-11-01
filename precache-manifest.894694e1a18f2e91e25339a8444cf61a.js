2752c2d83ccce91737cff47e78c2d14b9ce68d4d
Lisset Torres <lisset2012@gmail.com>
2018-11-01 15:25:15 -0400

    subiendo el build

---
+++ precache-manifest.894694e1a18f2e91e25339a8444cf61a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ef0cb1bee71b2c6fd8ac",
+    "url": "/static/js/main.ef0cb1be.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "ef0cb1bee71b2c6fd8ac",
+    "url": "/static/css/main.d08fc57a.chunk.css"
+  },
+  {
+    "revision": "242182d22af92e1085f2010daff7b4fb",
+    "url": "/index.html"
+  }
+];
